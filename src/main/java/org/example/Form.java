15db761f01b72db22953b1836ce42f9b7b6cc1dd
mikolajgradecki <mikolajgradecki00@gmail.com>
2023-11-06 18:59:44 +0100

    Ask how many users

---
+++ src/main/java/org/example/Form.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Form {
+
+    public static void amount (){
+        System.out.println(" Ile osób potrzebujesz? ");
+        Scanner scanner = new Scanner(System.in);
+        int amountPeople = scanner.nextInt();
+    }
+    //int amountPeople;
+
+
+}
